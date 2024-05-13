bc4e2bb7c98a5db7e4e6eea3d4c2deadefed5e95
h3184247758 <3184247758@qq.com>
2024-05-13 22:52:21 +0800

    职位画像小人图和雷达图

--- front/src/components/compareTitles.vue
+++ front/src/components/compareTitles.vue
@@ -1,14 +1,7 @@
 <template>
+  <div>
     <div class="container">
-      <el-select v-model="selectedJobTitle1" placeholder="请选择或输入职位" clearable filterable remote :remote-method="getCheckBoxData" class="select1">
-        <el-option
-          v-for="item in jobTitles"
-          :key="item"
-          :label="item"
-          :value="item"
-        ></el-option>
-      </el-select>
-      <el-select v-model="selectedJobTitle2" placeholder="请选择或输入职位" clearable filterable remote :remote-method="getCheckBoxData" class="select2">
+      <el-select v-model="selectedJobTitle" placeholder="请选择或输入职位" clearable filterable remote :remote-method="getCheckBoxData" class="select">
         <el-option
           v-for="item in jobTitles"
           :key="item"
@@ -18,83 +11,138 @@
       </el-select>
       <el-button class="search-button" type="primary" @click="search">搜索</el-button>
     </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        jobTitles: [],
-        selectedJobTitle1: '',
-        selectedJobTitle2: ''
-      };
+    <div class="charts-container">
+      <percentageChart ref="percentageChart" style="width: 30%; height: 300px;"/>
+      <div ref="radarChart" style="width: 500px; height: 300px;"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import * as echarts from 'echarts'; 
+import percentageChart from './percentageChart.vue';
+
+export default {
+  components: {
+    percentageChart
+  },
+  data() {
+    return {
+      jobTitles: [],
+      skillPreference: [0.5, 0.5, 0.5],
+      avgSkillPreference: [0.3, 0.3, 0.3],
+      selectedJobTitle: '',
+      radarChart: null,
+      typeName: '',
+      searchedSalary: 0
+    };
+  },
+  mounted() {
+    this.typeName = 'type_MpUmNW';
+    this.getCheckBoxData();
+    this.radarChart = echarts.init(this.$refs.radarChart);
+    this.renderRadarChart();
+    window.addEventListener('storage', e => {
+        if(e.key == 'selectedJobTitle'){
+            console.log("new")
+        }
+      })
+  },
+  methods: {
+    getCheckBoxData() {
+      axios.post('http://127.0.0.1:5000/check_box', {
+        typeName: this.typeName,
+      })
+        .then(response => {
+          this.jobTitles = response.data;
+        })
+        .catch(error => {
+          console.error('Error fetching checkbox data:', error);
+        });
     },
-    mounted() {
-      this.getCheckBoxData();
-      this.restoreFromLocalStorage();
-    },
-    methods: {
-      getCheckBoxData() {
-        axios.get('http://127.0.0.1:5000/check_box')
-          .then(response => {
-            this.jobTitles = response.data.job_titles;
-          })
-          .catch(error => {
-            console.error('Error fetching checkbox data:', error);
-          });
-      },
-      search() {
-        if (this.selectedJobTitle1 === '') {
-          this.selectedJobTitle1 = 'e0dd920456695914fed9481503e83b41xW';
-        }
-        if (this.selectedJobTitle2 === '') {
-          this.selectedJobTitle2 = '9f17193f3306b63788eac8e6a4b70f88oL';
-        }
-        axios.post('http://127.0.0.1:5000/job_title_comparison', {
-            jobTitle1: this.selectedJobTitle1,
-            jobTitle2: this.selectedJobTitle2
-          })
-          .then(response => {
-            console.log(response);
-          })
-          .catch(error => {
-            console.error('Error sending job title comparison data:', error);
-          });
-        localStorage.setItem('selectedJobTitle1', this.selectedJobTitle1);
-        localStorage.setItem('selectedJobTitle2', this.selectedJobTitle2);
-      },
-      restoreFromLocalStorage() {
-        const jobTitle1 = localStorage.getItem('selectedJobTitle1');
-        const jobTitle2 = localStorage.getItem('selectedJobTitle2');
-        if (jobTitle1) {
-          this.selectedJobTitle1 = jobTitle1;
-        }
-        if (jobTitle2) {
-          this.selectedJobTitle2 = jobTitle2;
-        }
+    search() {
+      if (this.selectedJobTitle === '') {
+        this.selectedJobTitle = 'e0dd920456695914fed9481503e83b41xW';
       }
+      axios.post('http://127.0.0.1:5000/job_title_comparison', {
+          jobTitle: this.selectedJobTitle,
+          typeName: this.typeName,
+        })
+        .then(response => {
+          this.skillPreference = response.data.matrix_job.flat().slice(0, 3);
+          this.avgSkillPreference = response.data.matrix_title.flat().slice(0, 3);
+          this.searchedSalary = response.data.salary;
+          this.renderRadarChart();
+        })
+        .catch(error => {
+          console.error('Error sending job title comparison data:', error);
+        });
+      this.$refs.percentageChart.updateChartOption();
+      sessionStorage.setItem('selectedJobTitle', this.selectedJobTitle);
+      localStorage.setItem('salaryPercentage', this.searchedSalary);
+    },
+    renderRadarChart() {
+      const option = {
+        radar: {
+          indicator: [
+            { name: 'City', max: 1 },
+            { name: 'Experience', max: 1 },
+            { name: 'Education', max: 1 },
+          ],
+          center: ['50%', '50%'],
+          radius: '60%'
+        },
+        series: [{
+          type: 'radar',
+          data: [
+            {
+              value: this.skillPreference, 
+              name: 'Skill Performance'
+            },
+            {
+              value: this.avgSkillPreference, 
+              name: 'Average Skill Performance'
+            }
+          ]
+        }]
+      };
+      this.radarChart.setOption(option);
     }
-  };
-  </script>
-  
-  <style>
-  .container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    max-width: 800px; /* 设置容器的最大宽度 */
-    margin: 0 auto; /* 水平居中 */
   }
-  
-  .select1,
-  .select2 {
-    width: 31%; /* 设置选择框的宽度 */
-  }
-  
-  .search-button {
-    width: 15%; /* 设置按钮的宽度 */
-  }
-  </style>
-  
+};
+</script>
+
+<style>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  max-width: 800px; /* 设置容器的最大宽度 */
+  margin: 0 auto; /* 水平居中 */
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.select {
+  width: 60%; /* 设置选择框的宽度 */
+}
+
+.search-button {
+  width: 30%; /* 设置按钮的宽度 */
+}
+
+.charts-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start; /* 将内容从容器顶部开始排列 */
+  position: absolute;
+  left: 0;
+  margin-left: 20px; /* 左边距 */
+  margin-top: 200px;
+}
+
+</style>
+
