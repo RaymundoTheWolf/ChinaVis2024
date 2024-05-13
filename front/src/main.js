34e7d42291b43d42ff9e94fba013c659047b5d5d
h3184247758 <3184247758@qq.com>
2024-05-13 15:39:48 +0800

    引入elementUI

--- front/src/main.js
+++ front/src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import dataV from '@jiaminghi/data-view'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
-Vue.use(dataV)
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
