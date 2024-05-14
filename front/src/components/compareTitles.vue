d4f008b07d8c6f4dfe410059b4b41a5904aea942
h3184247758 <3184247758@qq.com>
2024-05-14 18:23:05 +0800

    职位画像图前后端完善

--- front/src/components/compareTitles.vue
+++ front/src/components/compareTitles.vue
@@ -11,9 +11,27 @@
       </el-select>
       <el-button class="search-button" type="primary" @click="search">搜索</el-button>
     </div>
-    <div class="charts-container">
-      <percentageChart ref="percentageChart" style="width: 30%; height: 300px;"/>
-      <div ref="radarChart" style="width: 500px; height: 300px;"></div>
+    <div class="content-container">
+      <div class="charts-container">
+        <percentageChart ref="percentageChart" style="width: 300px; height: 300px;"/>
+        <div ref="radarChart" style="width: 300px; height: 400px;"></div>
+      </div>
+      <div class="final-container">
+        <div class="details-container">
+          <div class='pie-chart' ref="pieChart" style="width: 400px; height: 300px;"></div>
+          <div class='bar-chart' ref="barChart" style="width: 700px; height: 300px;"></div>
+        </div>
+        <div class="top-jobs-container">
+          <div
+            v-for="(job, index) in topJobs"
+            :key="index"
+            class="job-name"
+            @click="selectJob(job)"
+          >
+            {{ job }}
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -35,7 +53,14 @@ export default {
       selectedJobTitle: '',
       radarChart: null,
       typeName: '',
-      searchedSalary: 0
+      searchedSalary: 0,
+      cityName: ['J722', 'Q598', 'H610', 'K115'],
+      cityTimes: [1, 1, 1, 6],
+      minSalary: 1000,
+      maxSalary: 10000,
+      topJobs: ['75321885b3a892ec010656b498fda861kW', 'c38f53b33c8355beed8af099908fb1d7JE', 'fb6e82ef0a1eb0f9e6d8196638d40408tu'],
+      topValues: [0.8, 0.6, 0.7],
+      ringChart: []
     };
   },
   mounted() {
@@ -43,13 +68,16 @@ export default {
     this.getCheckBoxData();
     this.radarChart = echarts.init(this.$refs.radarChart);
     this.renderRadarChart();
-    window.addEventListener('storage', e => {
-        if(e.key == 'selectedJobTitle'){
-            console.log("new")
-        }
-      })
+    this.pieChart = echarts.init(this.$refs.pieChart);
+    this.renderPieChart();
+    this.barChart = echarts.init(this.$refs.barChart);
+    this.renderBarChart();
   },
   methods: {
+    selectJob(job) {
+      this.selectedJobTitle = job;
+      this.search();
+    },
     getCheckBoxData() {
       axios.post('http://127.0.0.1:5000/check_box', {
         typeName: this.typeName,
@@ -68,16 +96,23 @@ export default {
       axios.post('http://127.0.0.1:5000/job_title_comparison', {
           jobTitle: this.selectedJobTitle,
           typeName: this.typeName,
+          jobList: this.jobTitles.flat()
         })
         .then(response => {
+          this.topJobs = []
           this.skillPreference = response.data.matrix_job.flat().slice(0, 3);
           this.avgSkillPreference = response.data.matrix_title.flat().slice(0, 3);
           this.searchedSalary = response.data.salary;
+          this.cityName = response.data.city_name;
+          this.cityTimes = response.data.city_times.flat()
+          this.maxSalary = response.data.max_salary;
+          this.minSalary = response.data.min_salary;
+          this.topJobs = response.data.top_jobs.flat();
           this.renderRadarChart();
+          this.renderPieChart();
+          this.renderBarChart();
+          console.log(this.topJobs)
         })
-        .catch(error => {
-          console.error('Error sending job title comparison data:', error);
-        });
       this.$refs.percentageChart.updateChartOption();
       sessionStorage.setItem('selectedJobTitle', this.selectedJobTitle);
       localStorage.setItem('salaryPercentage', this.searchedSalary);
@@ -108,8 +143,72 @@ export default {
         }]
       };
       this.radarChart.setOption(option);
-    }
-  }
+    },
+    renderPieChart() {
+      this.pieChart.clear();
+      const option = {
+        tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b}: {c} ({d}%)'
+        },
+        series: [
+          {
+            name: 'Access From',
+            type: 'pie',
+            radius: ['40%', '70%'],
+            avoidLabelOverlap: false,
+            padAngle: 5,
+            itemStyle: {
+              borderRadius: 10
+            },
+            label: {
+              show: false,
+              position: 'center'
+            },
+            emphasis: {
+              label: {
+                show: true,
+                fontSize: 40,
+                fontWeight: 'bold'
+              }
+            },
+            labelLine: {
+              show: false
+            },
+            data: this.cityName.map((name, index) => ({
+              value: this.cityTimes[index],
+              name: name
+            }))
+          }
+        ]
+      };
+      this.pieChart.setOption(option);
+    },
+    renderBarChart() {
+      this.barChart.clear();
+      const option = {
+        xAxis: {
+          type: 'value',
+          boundaryGap: [0, 0.01]
+        },
+        yAxis: {
+          type: 'category',
+          data: ['Min Salary', 'Max Salary']
+        },
+        series: [
+          {
+            type: 'bar',
+            data: [this.minSalary, this.maxSalary],
+            itemStyle: {
+              color: '#aba08c'
+            },
+            barWidth: '50%'
+          }
+        ]
+      };
+      this.barChart.setOption(option);
+    },
+  },
 };
 </script>
 
@@ -122,6 +221,7 @@ export default {
   margin: 0 auto; /* 水平居中 */
   position: absolute;
   left: 50%;
+  margin-top: 100px;
   transform: translateX(-50%);
 }
 
@@ -133,16 +233,46 @@ export default {
   width: 30%; /* 设置按钮的宽度 */
 }
 
+.content-container {
+  display: flex;
+  margin-left: 200px; /* 左边距 */
+  justify-content: space-between;
+}
+
 .charts-container {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: flex-start; /* 将内容从容器顶部开始排列 */
-  position: absolute;
   left: 0;
   margin-left: 20px; /* 左边距 */
+  margin-right: -40px; 
+  margin-top: 250px;
+}
+
+.details-container {
+  display: flex;
+  left: 0;
   margin-top: 200px;
 }
 
+.bar-chart{
+  margin-left: 10px
+}
+
+.final-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start; /* 将内容从容器顶部开始排列 */
+}
+
+.job-name {
+  font-size: 50px;
+  font-weight: bold;
+  font-family: 'cursive'; /* 设置花字字体 */
+  color: #9894a7;
+  margin-top: 50px;
+}
 </style>
 
