bcb56c1283231c1d31c8083303c680b874697675
Antlers <1217676835@qq.com>
2024-05-15 01:50:32 +0800

    update 3D scatter code

--- front/src/components/scatterChart.vue
+++ front/src/components/scatterChart.vue
@@ -64,11 +64,12 @@
 			}
 		  },
 		  visualMap: {
-			calculable: true,
-			dimension: 3,
-			max: maxSalary,
+			type: 'continuous', // 连续型
+			dimension: 3, // 对工资这一维度进行映射
+			min: minSalary, // 工资的最小值
+			max: maxSalary, // 工资的最大值
 			inRange: {
-			  symbolSize: [minSymbolSize, maxSymbolSize]
+			  color: ['#FF0000', '#00FF00', '#0000FF'] // 低工资为红色，中工资为绿色，高工资为蓝色
 			},
 			textStyle: {
 			  color: '#fff'
