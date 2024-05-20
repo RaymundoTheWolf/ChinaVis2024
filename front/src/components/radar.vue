f8d7ea430a9a8003f159a7d45271fe5dbad159c1
h3184247758 <3184247758@qq.com>
2024-05-20 20:01:16 +0800

    雷达图单列，修改职业画像

---
+++ front/src/components/radar.vue
@@ -0,0 +1,76 @@
+<template>
+    <div id="radar-chart">
+        <div ref="radarChart" class="radarChart"></div>
+    </div>
+</template>
+  
+  <script>
+  import * as echarts from 'echarts'; 
+  import { EventBus } from './eventBus';
+  
+  export default {
+    data() {
+      return {
+        jobTitles: [],
+        skillPreference: [0.5, 0.5, 0.5],
+        avgSkillPreference: [0.3, 0.3, 0.3],
+      };
+    },
+    mounted() {
+      this.radarChart = echarts.init(this.$refs.radarChart);
+      this.renderRadarChart();
+      EventBus.$on('radar-data', (data) => {
+            this.avgSkillPreference = data.avgSkillPreference;
+            this.skillPreference = data.skillPreference;
+            this.renderRadarChart()
+        });
+    },
+    methods: {
+      renderRadarChart() {
+        console.log("收到")
+        const option = {
+          radar: {
+            indicator: [
+              { name: 'City', max: 1 },
+              { name: 'Experience', max: 1 },
+              { name: 'Education', max: 1 },
+            ],
+            center: ['50%', '50%'],
+            radius: '60%'
+          },
+          series: [{
+            type: 'radar',
+            data: [
+              {
+                value: this.skillPreference, 
+                name: 'Skill Performance'
+              },
+              {
+                value: this.avgSkillPreference, 
+                name: 'Average Skill Performance'
+              }
+            ]
+          }]
+        };
+        this.radarChart.setOption(option);
+      },
+    }
+}
+  </script>
+  
+<style>
+
+#radar-chart{
+    width: 100%;
+    height: 100%;
+}
+  
+  
+#radar-chart .radarChart {
+    width: 100%;
+    height: 100%;
+}
+
+</style>
+  
+  
